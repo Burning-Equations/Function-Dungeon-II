be721cd13433a53f1132cf72e9d17156be19eef3
kian <kianniek@hotmail.com>
2024-06-10 13:59:00 +0200

    Added scenes

---
+++ Assets/Scripts/Editor/Custom/WaveManagerEditor.cs
@@ -0,0 +1,11 @@
+﻿using UnityEditor;
+using WaveSystem;
+
+namespace Editor.Custom
+{
+    [CustomEditor(typeof(WaveManager), true)]
+    public class WaveManagerEditor
+    {
+        //TODO: Implement custom editor for WaveManager
+    }
+}
